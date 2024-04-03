c30da92916d60a1bc9af8611900524a1469bedb9
anshchovatiya <sqanshchovatiya800@gmail.com>
2024-04-03 18:44:04 +0530

    dh

--- 29_circular_doubly_ended_link_list.c
+++ 29_circular_doubly_ended_link_list.c
@@ -44,7 +44,7 @@ void Print_list()
     printf("\n");
     if (Head == NULL)
     {
-        printf("list is empty");
+        printf("list is empty\n\n");
     }
 
     else
@@ -63,20 +63,19 @@ void Print_list()
 
 void Delete_back()
 {
-   
+
     struct Node *ptr = Head;
-    struct Node*p = NULL;
-    
-    while(ptr->next!=Head)
+    struct Node *p = NULL;
+
+    while (ptr->next != Head)
     {
         p = ptr;
-        ptr= ptr->next;
+        ptr = ptr->next;
     }
-    
+
     p->next = Head;
-    Head->previous= p;
+    Head->previous = p;
     free(ptr);
-   
 }
 
 void Insert_front(int number)
@@ -85,27 +84,110 @@ void Insert_front(int number)
     struct Node *temp = (struct Node *)malloc(sizeof(struct Node));
     temp->data = number;
 
-    if (Head == NULL )
+    if (Head == NULL)
     {
         Head = temp;
         temp->next = Head;
     }
     else
     {
-        temp->next = Head;
-        Head->previous = temp;
-        Head = temp;
 
-        
         struct Node *ptr = Head;
 
         while (ptr->next != Head)
         {
             ptr = ptr->next;
-                 printf("dxh");
         }
-        ptr->next = Head;
+
+        ptr->next = temp;
+
+        temp->next = Head;
+        Head->previous = temp;
+        Head = temp;
     }
+}
+
+void Delete_front()
+{
+    if (Head == NULL)
+    {
+        printf("List is Empty");
+    }
+    else
+    {
+        if (Head->next == Head)
+        {
+            Head = NULL;
+            return;
+        }
+        else
+        {
+            struct Node *ptr = Head;
+
+            while (ptr->next != Head)
+            {
+                ptr = ptr->next;
+            }
+
+            struct Node *p = Head;
+            Head = Head->next;
+            ptr->next = Head;
+            Head->previous = ptr;
+            free(p);
+        }
+    }
+}
+
+void Insert_middle(int number, int position)
+{
+    struct Node *temp = (struct Node *)malloc(sizeof(struct Node));
+    temp->data = number;
+
+    struct Node *ptr = Head;
+
+    while (ptr->data != position && ptr->next != Head)
+    {
+        ptr = ptr->next;
+    }
+
+    struct Node *p = ptr->next;
+
+    ptr->next = temp;
+    p->previous = temp;
+    temp->previous = ptr;
+    temp->next = p;
+}
+
+void Delete_mid(int position)
+{
+    struct Node *ptr = Head;
+
+    while (ptr->data != position && ptr->next != Head)
+    {
+        ptr = ptr->next;
+    }
+
+    if (ptr == Head)
+    {
+        struct Node *ptr = Head;
+
+        while (ptr->next != Head)
+        {
+            ptr = ptr->next;
+        }
+
+        struct Node*p = Head;
+        Head = Head->next;
+        Head->previous=ptr;
+        ptr->next=Head;
+        free(p);
+        return;
+    }
+
+    struct Node *p = ptr;
+    ptr->previous->next = ptr->next;
+    ptr->next->previous = ptr->previous;
+    free(p);
 }
 
 int main()
@@ -117,10 +199,10 @@ int main()
     Insert_back(5);
     Print_list();
 
-    Insert_front(58);
-   
-    Insert_front(6);
-    Insert_front(7);
-
+    Delete_mid(5);
+    Print_list();
+    Delete_mid(1);
+    Print_list();
+    Delete_mid(2);
     Print_list();
 }
